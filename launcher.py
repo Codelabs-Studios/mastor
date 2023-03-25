3659ce29984fb54678f6d63610ab4e2961066701
Cryxyemi <maiks0809@gmail.com>
2023-03-25 17:20:38 +0100

    dsfdsfdsfds

---
+++ launcher.py
@@ -0,0 +1,10 @@
+import discord
+import mikocord as mc
+
+bot = mc.Bot(intents=discord.Intents.all())
+
+if __name__ == "__main__":
+    bot.load_cogs("events")
+    bot.load_cogs("extensions", subdirectory=True)
+
+    bot.run()
